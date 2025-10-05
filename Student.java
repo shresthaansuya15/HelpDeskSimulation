d16eb4ad74bf95d3913facdd26255a2ad022c82a
Ansuya Shrestha <shrestha.ansuya15@gmail.com>
2025-10-05 15:58:26 +0000

    completed student class

--- Student.java
+++ Student.java
@@ -1,3 +1,51 @@
-public class Student {
-    
+public class Student 
+{
+    private String name;
+    private int course;
+    private int workload;
+    private int arrivalTime;
+
+    public Student(String name, int course, int workload, int arrivalTime) 
+    {
+        this.name = name;
+        this.course = course;
+        this.workload = workload;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public int getCourse() 
+    {
+        return course;
+    }
+
+    public int getWorkload() 
+    {
+        return workload;
+    }
+
+    public int getArrivalTime() 
+    {
+        return arrivalTime;
+    }
+
+    public int getCourseLevel()
+    {
+        int courseLevel = (course / 100) * 100;
+
+        if (courseLevel < 100)
+        {
+            return 100;
+        }
+        else if (courseLevel > 400)
+        {
+            return 400;
+        }
+
+        return courseLevel;    
+    }    
 }
