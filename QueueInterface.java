6c29d72ba8dd518d8aaf5a4efea36a4b86683c60
Ansuya Shrestha <shrestha.ansuya15@gmail.com>
2025-10-05 15:47:00 +0000

    completed queue interface

--- QueueInterface.java
+++ QueueInterface.java
@@ -1,3 +1,8 @@
-public class QueueInterface {
-    
+public interface QueueInterface<T> 
+{
+    public void enqueue(T element);
+    public T dequeue();
+    public boolean isFull();
+    public boolean isEmpty();
+    public Integer length();    
 }
